7a43b02b11547f3bc6b7284e0881398c4080d7b7
Max <zockerstollrak@web.de>
2017-10-21 12:56:18 +0200

    update

---
+++ Assets/Scripts/CamSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour {
+
+    public Camera Camerak;
+    private Camera camerat;
+
+    public Camera Camerat { get => camerat; set => camerat = value; }
+
+    // Use this for initialization
+    void Start () {
+        if (Camerak.enabled == true)
+        {
+            Camerak = false;
+            Camerat = true;
+        } else
+        {
+            Camerak = true;
+            Camerat = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
