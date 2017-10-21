636dc99fce04af623f659faabb5df75dba23d007
Max <zockerstollrak@web.de>
2017-10-21 09:16:20 +0200

    first commit of the whole ProjectSettings

---
+++ Assets/Scripts/CloseGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseGame : MonoBehaviour {
+
+        public void doquit()
+    {
+        Debug.Log("has quit the game");
+        Application.Quit();
+    }
+    }
