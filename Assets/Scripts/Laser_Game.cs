3ed1b0b851e38b8619bef633334bc087b072b4fa
Schargrat <bastianvomdach@gmx.de>
2017-10-22 16:41:04 +0200

    total cleanup

---
+++ Assets/Scripts/Laser_Game.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser_Game : MonoBehaviour {
+    private bool istrigger = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (istrigger == true)
+        {
+            StartCoroutine(Waitforlaser());
+        }
+	}
+    IEnumerator Waitforlaser()
+    {
+        yield return new WaitForSeconds(3);
+        GameObject.Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/laserWand.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserWand : MonoBehaviour {
+    public int gamelive = 3;
+    public int enemylive = 3;
+    public bool game = true;
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(trydodge());
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (gamelive <= 0)
+        {
+            Debug.Log("player tod");
+        }
+        if (enemylive <= 0)
+        {
+            Debug.Log("gegner tod");
+        }
+        
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "laser")
+        {
+            gamelive--;
+            Debug.Log("aua");
+        }
+    }
+    IEnumerator trydodge()
+    {
+        yield return new WaitForSeconds(3);
+        if (Random.value > 0.6)
+        {
+            enemylive--;
+        }
+
+    }
+}
